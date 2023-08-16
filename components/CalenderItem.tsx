d7d5e0486ebcfb7ba11a1972396f6c206295d636
nijimajohn <hiroki_kotera@pj100.biz>
2023-08-16 17:41:07 +0900

    カレンダー追加

--- components/CalenderItem.tsx
+++ components/CalenderItem.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import React from "react";
 
@@ -11,6 +11,13 @@ import getDay from "date-fns/getDay";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { useState } from "react";
 import DatePicker from "react-datepicker";
+
+interface AllEvents {
+  title: string;
+  allDay: boolean;
+  start: Date;
+  end: Date;
+}
 
 const locales = {
   ja: require("date-fns/locale/ja"),
@@ -44,9 +51,24 @@ const events = [
   },
 ];
 
-function CalenderItem() {
+const CalenderItem = () => {
+  const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
+  const [allEvents, setAllEvents] = useState<any>(events);
+
+  const handleAddEvent = () => {
+    setAllEvents([...allEvents, newEvent]);
+  };
+
   return (
     <div className="App">
+      <input
+        type="text"
+        placeholder="タイトル"
+        style={{ width: "20%", marginRight: "10px" }}
+        value={newEvent.title}
+        onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })}
+      />
+      <button onClick={() => {handleAddEvent()}}>test</button>
       <Calendar
         localizer={localizer}
         events={events}
@@ -54,8 +76,9 @@ function CalenderItem() {
         endAccessor="end"
         style={{ height: 500, margin: "50px" }}
       />
+      
     </div>
   );
-}
+};
 
 export default CalenderItem;
